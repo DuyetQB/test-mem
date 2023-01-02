c8c1ea9093ee99d26c0f7ab56f3eb55a011523a5
DESKTOP-RONIHRQ\Legion 5 <dinhsyduyet@gmail.com>
2023-01-02 22:47:26 +0700

    fix bug npm

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,14 +7,15 @@ import Container from 'react-bootstrap/Container';
 import 'aos/dist/aos.css';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render( <React.StrictMode>
 
-    <Container fluid='true' >
+  <Container fluid='true' >
       <App />
     </Container>
-
+</React.StrictMode>,
+ document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
